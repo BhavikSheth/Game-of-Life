12e73d251b6d6d65e311aecb97e1188a77fa2b57
Bhavik Sheth <bhaviksheth20@gmail.com>
2018-02-22 19:09:36 +0000

    add prop-types module and build board row

---
+++ src/components/boardRow.js
@@ -0,0 +1,26 @@
+/* eslint-disable */
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import Cell from "./cell";
+
+class BoardRow extends Component {
+  // renderCell = (row, column) => <Cell row={row} column={column} />
+  render() {
+    // console.log("test", this.props.rowIndex, typeof this.props.rowIndex);
+    // console.log(this.props);
+    return (
+      <div className="board-row">
+        {
+          // Array.from({ length: 50 }, (cell, index) => this.renderCell(this.props.rowIndex, index))
+          Array.from({length: 50 }, (column, index) => <Cell key={index} rowIndex={this.props.rowIndex} columnIndex={index}/>)
+        }
+      </div>
+    );
+  }
+}
+
+BoardRow.propTypes = {
+  rowIndex: PropTypes.number.isRequired,
+};
+
+export default BoardRow;
