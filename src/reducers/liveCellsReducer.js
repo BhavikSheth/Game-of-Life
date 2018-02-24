f6e394c3f8c8f3f2127098b6e3f4f87933f94c5e
Bhavik Sheth <bhaviksheth20@gmail.com>
2018-02-24 09:02:25 +0000

    add EVOLVE_CELLS in reducer and test

--- src/reducers/liveCellsReducer.js
+++ src/reducers/liveCellsReducer.js
@@ -1,4 +1,4 @@
-import { ADD_LIVE_CELL, DELETE_LIVE_CELL } from "../constants/actionTypes";
+import { ADD_LIVE_CELL, DELETE_LIVE_CELL, EVOLVE_CELLS } from "../constants/actionTypes";
 
 export default (state = {}, action) => {
   const { row, column, type } = action;
@@ -14,6 +14,78 @@ export default (state = {}, action) => {
         return Object.assign({}, newState);
       }
       return { ...state, [row]: state[row].filter(liveColumnCell => liveColumnCell !== column) };
+    case EVOLVE_CELLS: {
+      const newGeneration = {};
+      for (let i = 0; i < 10; i += 1) {
+        for (let j = 0; j < 10; j += 1) {
+          const rowAbove = (i === 0 ? 10 - 1 : i - 1);
+          const rowBelow = (i === 10 - 1 ? 0 : i + 1);
+          const columnLeft = (j === 0 ? 10 - 1 : j - 1);
+          const columnRight = (j === 10 - 1 ? 0 : j + 1);
+          const cellIsAlive = Boolean(state[i] && state[i].indexOf(j) !== -1);
+          const neighbours =
+            Boolean(state[rowAbove] && state[rowAbove].indexOf(columnLeft) !== -1)
+            + Boolean(state[rowAbove] && state[rowAbove].indexOf(j) !== -1)
+            + Boolean(state[rowAbove] && state[rowAbove].indexOf(columnRight) !== -1)
+            + Boolean(state[i] && state[i].indexOf(columnLeft) !== -1)
+            + Boolean(state[i] && state[i].indexOf(columnRight) !== -1)
+            + Boolean(state[rowBelow] && state[rowBelow].indexOf(columnLeft) !== -1)
+            + Boolean(state[rowBelow] && state[rowBelow].indexOf(j) !== -1)
+            + Boolean(state[rowBelow] && state[rowBelow].indexOf(columnRight) !== -1);
+          // console.log(state, i, j, rowAbove, rowBelow, columnLeft, columnRight);
+          // console.log(cellIsAlive, neighbours);
+          // console.log((state[rowAbove] && state[rowAbove].indexOf(columnLeft) !== -1));
+          // console.log((state[rowAbove] && state[rowAbove].indexOf(j) !== -1));
+          // console.log((state[rowAbove] && state[rowAbove].indexOf(columnRight) !== -1));
+          // console.log((state[i] && state[i].indexOf(columnLeft) !== -1));
+          // console.log((state[i] && state[i].indexOf(columnRight) !== -1));
+          // console.log((state[rowBelow] && state[rowBelow].indexOf(columnLeft) !== -1));
+          // console.log((state[rowBelow] && state[rowBelow].indexOf(j) !== -1));
+          // console.log((state[rowBelow] && state[rowBelow].indexOf(columnRight) !== -1));
+          
+          // if (cellIsAlive && neighbours < 2) {
+          //   // if (state[i].length === 1) {
+          //   //   const newState = Object.keys(state).reduce((result, key) => (key !== i.toString() ? { ...result, [key]: state[key] } : result), {});
+          //   //   return Object.assign({}, newState);
+          //   // }
+          //   // return { ...state, [i]: state[i].filter(liveColumnCell => liveColumnCell !== j) };
+          // }
+
+          if (cellIsAlive && (neighbours === 2 || neighbours === 3)) {
+            // console.log("test1");
+            // console.log(state, i, j, rowAbove, rowBelow, columnLeft, columnRight);
+            // console.log(cellIsAlive, neighbours);
+            newGeneration[i] = newGeneration[i] ? [...newGeneration[i], j] : [j];
+            // console.log(newGeneration);
+          }
+
+          if (!cellIsAlive && neighbours === 3) {
+            // console.log("test2");
+            // console.log(state, i, j, rowAbove, rowBelow, columnLeft, columnRight);
+            // console.log(cellIsAlive, neighbours);
+            newGeneration[i] = newGeneration[i] ? [...newGeneration[i], j] : [j];
+            // console.log(newGeneration);            
+          }
+
+          // if (cellIsAlive && neighbours > 3) {
+          //   if (state[i].length === 1) {
+          //     const newState = Object.keys(state).reduce((result, key) => (key !== i.toString() ? { ...result, [key]: state[key] } : result), {});
+          //     return Object.assign({}, newState);
+          //   }
+          //   return { ...state, [i]: state[i].filter(liveColumnCell => liveColumnCell !== j) };
+          // }
+
+          // if (!cellIsAlive && neighbours === 3) {
+          //   if (state[i]) {
+          //     return { ...state, [i]: [...state[i], j].sort((a, b) => a - b) };
+          //   }
+          //   return { ...state, [i]: [j] };
+          // }
+        }
+      }
+      // console.log(newGeneration);
+      return newGeneration;
+    }
     default:
       return state;
   }
--- src/reducers/liveCellsReducer.spec.js
+++ src/reducers/liveCellsReducer.spec.js
@@ -1,29 +1,76 @@
 import { expect } from "chai";
 import liveCellsReducer from "./liveCellsReducer";
-import { ADD_LIVE_CELL, DELETE_LIVE_CELL } from "../constants/actionTypes";
+import { ADD_LIVE_CELL, DELETE_LIVE_CELL, EVOLVE_CELLS } from "../constants/actionTypes";
 
 describe("Live Cells Reducer", () => {
   it("handles action with unknown type", () => {
     expect(liveCellsReducer(undefined, {})).to.eql({});
   });
 
-  it("handles action of type ADD_LIVE_CELL when the row does not exist", () => {
+  it("ADD_LIVE_CELL: creates new row when row does not exist", () => {
     const action = { type: ADD_LIVE_CELL, row: 1, column: 2 };
     expect(liveCellsReducer({}, action)).to.eql({ 1: [2] });
   });
 
-  it("handles action of type ADD_LIVE_CELL when the row does exist", () => {
+  it("ADD_LIVE_CELL: adds new column to row when row does exist", () => {
     const action = { type: ADD_LIVE_CELL, row: 1, column: 2 };
     expect(liveCellsReducer({ 1: [3, 4] }, action)).to.eql({ 1: [2, 3, 4] });
   });
 
-  it("handles action of type DELETE_LIVE_CELL when length of column array is 1", () => {
+  it("DELETE_LIVE_CELL: deletes row when length of column array of row is 1", () => {
     const action = { type: DELETE_LIVE_CELL, row: 1, column: 2 };
     expect(liveCellsReducer({ 1: [2] }, action)).to.eql({});
   });
 
-  it("handles action of type DELETE_LIVE_CELL when length of column array is more than 1", () => {
+  it("DELETE_LIVE_CELL: deletes column when length of column array of row is more than 1", () => {
     const action = { type: DELETE_LIVE_CELL, row: 1, column: 2 };
     expect(liveCellsReducer({ 1: [1, 2, 3, 4] }, action)).to.eql({ 1: [1, 3, 4] });
+  });
+
+  it("EVOLVE_CELLS: live cell with fewer than two live neighbours dies", () => {
+    const action = { type: EVOLVE_CELLS };
+    expect(liveCellsReducer({ 1: [0] }, action)).to.eql({});
+    expect(liveCellsReducer({ 1: [0, 1] }, action)).to.eql({});
+    expect(liveCellsReducer({ 1: [0, 1, 2] }, action)[1].indexOf(0)).to.equal(-1);
+    expect(liveCellsReducer({ 1: [0, 1, 2] }, action)[1].indexOf(2)).to.equal(-1);
+    expect(liveCellsReducer({ 1: [1], 2: [1], 3: [1] }, action)).to.not.have.property(1);
+    expect(liveCellsReducer({ 1: [1], 2: [1], 3: [1] }, action)).to.not.have.property(3);
+  });
+
+  it("EVOLVE_CELLS: live cell with two or three live neighbours lives", () => {
+    const action = { type: EVOLVE_CELLS };
+    expect(liveCellsReducer({ 1: [0, 1, 2] }, action)[1].indexOf(1)).to.not.equal(-1);
+    expect(liveCellsReducer({ 1: [0, 1], 2: [1] }, action)[1].indexOf(1)).to.not.equal(-1);
+    expect(liveCellsReducer({ 1: [0, 1], 2: [2] }, action)[1].indexOf(1)).to.not.equal(-1);
+    expect(liveCellsReducer({ 1: [1, 2], 2: [1, 2] }, action)).to.eql({ 1: [1, 2], 2: [1, 2] });
+  });
+
+  it("EVOLVE_CELLS: live cell with more than three live neighbours dies", () => {
+    const action = { type: EVOLVE_CELLS };
+    expect(liveCellsReducer({ 1: [1, 2, 3], 2: [1, 2] }, action)[2].indexOf(1)).to.not.eql(-1);
+    expect(liveCellsReducer({ 1: [1, 2, 3], 2: [1, 2] }, action)[2].indexOf(3)).to.not.eql(-1);
+    expect(liveCellsReducer({ 1: [1, 2, 3], 2: [1, 2] }, action)[2]).to.eql([1, 3]);
+    expect(liveCellsReducer({ 1: [1, 2, 3], 2: [1, 2, 3], 3: [1, 2, 3] }, action)[2].indexOf(2)).to.eql(-1);
+  });
+
+  it("EVOLVE_CELLS: dead cell with exactly three live neighbours becomes a live cell", () => {
+    const action = { type: EVOLVE_CELLS };
+    expect(liveCellsReducer({ 1: [0, 1, 2] }, action)[2].indexOf(1)).to.not.equal(-1);
+    expect(liveCellsReducer({ 1: [1, 3], 3: [1] }, action)[2].indexOf(2)).to.not.equal(-1);
+  });
+
+  it("EVOLVE_CELLS: testing for correct new generation", () => {
+    const action = { type: EVOLVE_CELLS };
+    expect(liveCellsReducer({ 1: [1, 2, 3], 2: [1, 2, 3], 3: [1, 2, 3] }, action)).to.eql({
+      0: [2], 1: [1, 3], 2: [0, 4], 3: [1, 3], 4: [2],
+    });
+  });
+
+  it("EVOLVE_CELLS: cells become alive and die at the opposite side of the board when at the edge of the game board", () => {
+    const action = { type: EVOLVE_CELLS };
+    expect(liveCellsReducer({ 0: [0, 1, 2] }, action)).to.eql({ 0: [1], 1: [1], 9: [1] });
+    expect(liveCellsReducer({ 0: [0, 8, 9], 8: [0, 8, 9], 9: [0, 8, 9] }, action)).to.eql({
+      0: [0, 8], 1: [9], 7: [9], 8: [0, 8], 9: [1, 7],
+    });
   });
 });
